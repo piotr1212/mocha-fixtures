b8d5cebab5d82c6a0a6562e5ff64dfa35a485f8b
Sebastian McKenzie <sebmck@gmail.com>
2015-07-25 03:27:53 +0100

    1.0.1

--- bin/mocha-fixture-dump.js
+++ bin/mocha-fixture-dump.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 
 var getFixtures = require("../index");
-var directory = process.argv[2] || process.cwd();
-var fixtures = getFixtures(directory + "/tests/fixtures");
+var fs          = require("fs");
+
+var directory         = process.argv[2] || process.cwd();
+var fixturesDirectory = directory + "/tests/fixtures";
+if (!fs.existsSync(fixturesDirectory)) return;
+
+var fixtures = getFixtures(fixturesDirectory);
 fs.writeFileSync(directory + "/test-fixtures.json", JSON.stringify(fixtures));
